7a59f4eb82603d933b20c0ad5a35172fcc399639
Durgaprasad-54 <rdkgamers555@gmail.com>
2025-06-23 20:49:29 +0530

    add patterns

---
+++ patterns/patterns.py
@@ -0,0 +1,38 @@
+#square
+def square(n):
+    for i in range(n):
+        for j in range(n):
+            print("*",end=" ")
+        print()
+#right angle pyramid       
+def righttri(n):
+    for i in range(n):
+        for j in range(i+1):
+            print("*",end=" ")
+        print()
+#inverted right pyramid
+def inverted(n):
+    for i in range(n):
+        for j in range(n-i):
+            print("*",end=" ")
+        print()
+ #number right angle triangle       
+def numrighttri(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print()
+def numrighttri2(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(i,end=" ")
+        print()
+
+
+#inverted number right angle triangle
+def invnum(n):
+    for i in range(1,n+1):
+        for j in range(1,(n+1)-i+1):
+            print(j,end=" ")
+        print()
+
