3bfa34747d7f10fa46c8b42b49712727f3e81c10
DurgaPrasad-54 <prasad8790237@gmail.com>
2025-08-07 09:46:26 +0530

    add

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -35,12 +35,5 @@ def invnum(n):
         for j in range(1,(n+1)-i+1):
             print(j,end=" ")
         print()
-def daimond(n):
-    for i in range(n):
-        for j in range(n-i):
-            print(" ",end=" ")
-        for j in range((2*i)+1):
-            print("*",end=" ")
-        print()
-daimond(10)
+
 
